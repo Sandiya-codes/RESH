57306884e93cd696f5ac948575321df43f869218
Sandiya-codes <sandhiyat0618@gmail.com>
2026-07-31 15:30:25 +0530

    sample file deleted

--- src/main/java/com/scrr/project/model/Sample.java
+++
@@ -1,9 +0,0 @@
-package com.scrr.project.model;
-
-public class Sample {
-	public static void main(String[] args) {
-		System.out.println("sample file");
-		System.out.println("file uploaded");
-	}
-
-}
