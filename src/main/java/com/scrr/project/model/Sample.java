76d24f486deda59402c8935924becfbb483f943a
Sandiya-codes <sandhiyat0618@gmail.com>
2026-07-31 15:29:14 +0530

    sample upload

--- src/main/java/com/scrr/project/model/Sample.java
+++ src/main/java/com/scrr/project/model/Sample.java
@@ -3,6 +3,7 @@ package com.scrr.project.model;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("sample file");
+		System.out.println("file uploaded");
 	}
 
 }
